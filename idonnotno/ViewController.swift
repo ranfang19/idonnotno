8b92394bfa943be5f15db129ffb04af46388688f
ranfang19 <ranfang19@gmail.com>
2021-08-24 19:21:42 +0800

    Initial Commit

---
+++ idonnotno/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  idonnotno
+//
+//  Created by Ran FANG on 2021/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
