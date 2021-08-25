0ec1eb8b5e6c59cb2a69c1cfbcae7c386e79f9ba
ranfang19 <ranfang19@gmail.com>
2021-08-25 22:42:28 +0800

    Add comments

--- idonnotno/ListViewController.swift
+++ idonnotno/ListViewController.swift
@@ -7,28 +7,26 @@
 
 import UIKit
 
+// DataSource ==> Provide data
+// Delegate ==> Receive UITableView events
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Delegate ==> Tells the delegate a row is selected
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("1111")
     }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
+    // DataSource ==> 返回row数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return list[p].count
     }
-    
+    // DataSource ==> 为每行提供cell。先通过identifier绑定cell，然后显示cell的相关信息
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text=list[p][indexPath.row
-        ]
+        cell.textLabel?.text=list[p][indexPath.row]
         return cell
     }
     
     @IBOutlet weak var nameSegmentedControl:UISegmentedControl!
-    
     @IBOutlet weak var tableView: UITableView!
     
     let list = [["🦆烤鸭鸭","🍗韩餐","🔥火锅","🥗沙拉","🍜拉面","🍡串串"],["⚠️不好吃的1","⚠️不好吃的2","⚠️不好吃的3","⚠️不好吃的4","⚠️不好吃的5","⚠️不好吃的6"]]
@@ -38,7 +36,6 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBAction func controlValueChanged(_ sender: UISegmentedControl) {
         p = sender.selectedSegmentIndex
         tableView.reloadData()
-
     }
 
     override func viewDidLoad() {
--- idonnotno/RandomViewController.swift
+++ idonnotno/RandomViewController.swift
@@ -9,13 +9,11 @@ import UIKit
 
 class RandomViewController: UIViewController {
     
-
     @IBOutlet weak var randomResultLabel: UILabel!
     @IBOutlet weak var randomResultButton: UIButton!
     
     let randomList = ["烤串","韩餐","日料","烤鸭","湘菜","川菜","东北菜","火锅","西餐","烤肉"]
 
-    
     @IBAction func randomAction(_ sender: Any) {
         randomResultLabel.text=getRandomList();
     }
@@ -24,12 +22,10 @@ class RandomViewController: UIViewController {
         return randomList[Int(arc4random_uniform(UInt32(randomList.count)))];
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
+   
 
 }
